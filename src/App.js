aef8173e15e84eef822fa15b53b52455083b2cc8
Gagandeep-N <gagandeep.n.2001@gmail.com>
2025-02-20 19:03:09 +0530

    feat(LandingPage): create Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Login from './components/Login/login';
 import PopUp from './components/PopUp/PopUp';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       {/* <Login/> */}
-      <PopUp/>
+      {/* <PopUp/> */}
+      <NavBar />
     </div>
   );
 }
